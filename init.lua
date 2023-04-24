cfad3a59443dd5f72e8142ab2c8947138a0e3d53
JoachimReiss <reiss@bryter.io>
2023-04-24 08:08:02 +0200

    setup that enables code navigation and formatting for work monorepo

--- init.lua
+++ init.lua
@@ -28,7 +28,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
+        enabled = false,    -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -68,33 +68,31 @@ return {
       mapping = { "bb", "jj", "kk" },
     }
 
-    -- VOLAR
-    require("lspconfig").volar.setup {
-      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
-    }
     local util = require "lspconfig.util"
-    local function get_typescript_server_path(root_dir)
-      -- local global_ts = "/home/[yourusernamehere]/.npm/lib/node_modules/typescript/lib
-      -- Alternative location if installed as root:
-      -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
-      local global_ts = "/usr/local/lib/node_modules/typescript/lib"
-      local found_ts = ""
-      local function check_dir(path)
-        found_ts = util.path.join(path, "node_modules", "typescript", "lib")
-        if util.path.exists(found_ts) then return path end
-      end
-      if util.search_ancestors(root_dir, check_dir) then
-        return found_ts
-      else
-        return global_ts
-      end
-    end
-    require("lspconfig").volar.setup {
-      on_new_config = function(new_config, new_root_dir)
-        new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-      end,
-    }
-
+    -- -- VOLAR
+    -- local function get_typescript_server_path(root_dir)
+    --   -- local global_ts = "/home/[yourusernamehere]/.npm/lib/node_modules/typescript/lib
+    --   -- Alternative location if installed as root:
+    --   -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+    --   local global_ts = "/usr/local/lib/node_modules/typescript/lib"
+    --   local found_ts = ""
+    --   local function check_dir(path)
+    --     found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+    --     if util.path.exists(found_ts) then return path end
+    --   end
+    --   if util.search_ancestors(root_dir, check_dir) then
+    --     return found_ts
+    --   else
+    --     return global_ts
+    --   end
+    -- end
+    -- require("lspconfig").volar.setup {
+    --   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue", "json" },
+    --   on_new_config = function(new_config, new_root_dir)
+    --     new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+    --   end,
+    -- }
+    --
     -- ESLINT
     require("lspconfig").eslint.setup {
       root_dir = util.root_pattern ".git",
